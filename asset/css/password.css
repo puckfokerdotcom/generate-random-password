1dc07e663b686ee7b02e8c862120009c62c8e9dc
puckfokerdotcom <115953212+puckfokerdotcom@users.noreply.github.com>
2023-02-08 18:39:18 -0800

    Create password.css

---
+++ asset/css/password.css
@@ -0,0 +1,25 @@
+
+.input-container {
+  display: flex;
+  align-items: center;
+}
+
+.password-label {
+  margin-left: 10px;
+}
+
+.lowercase {
+  color: blue;
+}
+
+.uppercase {
+  color: yellow;
+}
+
+.number {
+  color: green;
+}
+
+.punctuation {
+  color: red;
+}
